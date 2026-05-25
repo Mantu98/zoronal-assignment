f8f746b7b672015b6e42a4beb50910bd74b12233
Mantu Kumar <shaddil.saifi@terranxt.com>
2026-05-25 11:48:32 +0530

    fix: resolve TypeScript build and export issues

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { createCompany, getCompanies, getCompanyById } from "../controllers/companyController";
 import validateRequest from "../middleware/validateRequest";
-import { companySchema } from "../validations/company.validation";
+import { createCompanySchema } from "../validations/company.validation";
 
 const router = express.Router();
 
 router.get("/",getCompanies);
 router.get("/:id",getCompanyById);
-router.post("/",validateRequest(companySchema), createCompany);
+router.post("/",validateRequest(createCompanySchema), createCompany);
 
 export default router;
